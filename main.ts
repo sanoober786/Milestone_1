4dc301927ca371c85fa564707bdfbf208e622597
sanoober <toobasadhayo454003@gmail.com>
2024-11-09 22:00:19 +0500

    pushing the code

---
+++ main.ts
@@ -0,0 +1,11 @@
+const button = document.getElementById('toggle-skills') as HTMLButtonElement
+
+const skills = document.getElementById('skill') as HTMLElement
+
+button.addEventListener('click', ()=> {
+    if(skills.style.display === 'none'){
+        skills.style.display = 'block'
+    } else {
+        skills.style.display = 'none'
+    }
+});
